739f468f1ca9e021837bc1ac78a23394528c608a
Jenny Zhang <Jenny.Zhang@ec.gc.ca>
2021-06-04 15:29:42 -0400

    set up i18next for internationalization

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,49 +1,45 @@
-import Link from 'next/Link'
-import styled from '@emotion/styled'
-import {
-    Flex,
-    Button,
-    Box
-} from '@chakra-ui/react'
+import Link from 'next/Link';
+import styled from '@emotion/styled';
+import { Flex, Button, Box } from '@chakra-ui/react';
+import { useTranslation } from 'react-i18next';
 
-import Logo from './Logo'
+import Logo from './Logo';
 
-const NavLink = ({ to, label, ...props}) => {
+const NavLink = ({ to, label, ...props }) => {
     return (
         <Link href={to}>
-            <Button as="a" >
-                {label}
-            </Button>
+            <Button as='a'>{label}</Button>
         </Link>
-    )
-}
+    );
+};
 
 const NavBar = (props) => {
+    const { t } = useTranslation();
     const NavContainer = styled(Flex)`
         position: sticky;
         z-index: 10;
         top: 0;
-    `
+    `;
     return (
         <NavContainer
-            as="nav"
-            flexDirection="row"
-            justifyContent="space-between"
-            alignItems="center"
-            width="100%"
-            maxWidth="1024px"
-            mx="auto"
+            as='nav'
+            flexDirection='row'
+            justifyContent='space-between'
+            alignItems='center'
+            width='100%'
+            maxWidth='1024px'
+            mx='auto'
             mt={8}
             mb={[0, 0, 8]}
         >
-            <Logo/>
+            <Logo />
             <Box>
-                <NavLink to="/" label="Home"/>
-                <NavLink to="/about" label="About Us"/>
-                <NavLink to="/contact" label="Contact Us"/>
+                <NavLink to='/' label='Home' />
+                <NavLink to='/about' label={t('about')} />
+                <NavLink to='/contact' label='Contact Us' />
             </Box>
         </NavContainer>
-    )
-}
+    );
+};
 
 export default NavBar;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,48 +1,50 @@
-import { AppProps } from 'next/app'
-import { ChakraProvider, useColorMode } from "@chakra-ui/react"
-import { Global, css } from '@emotion/react'
+import { AppProps } from 'next/app';
+import { ChakraProvider, useColorMode } from '@chakra-ui/react';
+import { Global, css } from '@emotion/react';
 
 /* Not sure if this is best way to add global styles */
-const GlobalStyle = ({ children }) => { 
-  const { colorMode } = useColorMode()
+const GlobalStyle = ({ children }) => {
+    const { colorMode } = useColorMode();
 
-  return (
-    <>
-      <Global
-        styles={css`
-          ::selection {
-            background-color: #90CDF4;
-            color: #fefefe;
-          }
-          ::-moz-selection {
-            background: #ffb7b7;
-            color: #fefefe;
-          }
-          html {
-            min-width: 356px;
-            scroll-behavior: smooth;
-          }
-          #__next {
-            display: flex;
-            flex-direction: column;
-            min-height: 100vh;
-            background: ${colorMode === 'light' ? 'white' : 'black'};
-          }
-        `}
-      />
-      {children}
-    </>
-  )
-}
+    return (
+        <>
+            <Global
+                styles={css`
+                    ::selection {
+                        background-color: #90cdf4;
+                        color: #fefefe;
+                    }
+                    ::-moz-selection {
+                        background: #ffb7b7;
+                        color: #fefefe;
+                    }
+                    html {
+                        min-width: 356px;
+                        scroll-behavior: smooth;
+                    }
+                    #__next {
+                        display: flex;
+                        flex-direction: column;
+                        min-height: 100vh;
+                        background: ${colorMode === 'light'
+                            ? 'white'
+                            : 'black'};
+                    }
+                `}
+            />
+            {children}
+        </>
+    );
+};
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <ChakraProvider>
-      <GlobalStyle>
-        <Component {...pageProps} />
-      </GlobalStyle>
-    </ChakraProvider>
-  )
+    return (
+        <ChakraProvider>
+            <GlobalStyle>
+                <Component {...pageProps} />
+            </GlobalStyle>
+        </ChakraProvider>
+    );
 }
 
-export default MyApp
+export default MyApp;
