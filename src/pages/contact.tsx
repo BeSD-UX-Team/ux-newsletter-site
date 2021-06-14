881ecc1f4634438943c2faf5da5bc8998ed613ee
Lam <Jessie.Lam@ec.gc.ca>
2021-06-14 12:39:52 -0400

    completed front end of contact us page

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head';
-import { Stack, Flex, Heading, Box, Text, Image, HStack, Center } from '@chakra-ui/react';
+import { Stack, Heading, Box, Text, Image, HStack, Center, Input, Textarea, Flex, Button} from '@chakra-ui/react';
 import { useTranslation } from 'react-i18next';
 
 import Container from '../components/Container';
@@ -24,13 +24,13 @@ export default function Contact() {
                 alignItems='flex-start'
                 m='0 20px 4rem 20px'
             >
-                <Flex w='100%' justifyContent='center' alignItems='flex-start'>
+                <Box w='100%' textAlign="center">
                     <Heading size="xl"> Contact Us</Heading>
-                </Flex>
+                </Box>
                 <Box w='100%'>
 
 
-                    <HStack w='100%' justify="center">
+                    <HStack w='100%' justify="center" flexWrap="wrap">
                         <Image src='/assets/Avatar5.png' alt='Ron Handley' boxSize="100px"/>
                         <Image src='/assets/Avatar4.png' alt='Aleksui Riabtsev' boxSize="100px"/>
                         <Image src='/assets/Avatar3.png' alt='Raied Siddiqui' boxSize="100px"/>
@@ -44,7 +44,7 @@ export default function Contact() {
                         pb={4}
                         textAlign = 'center'
                     >
-                        What we do
+                        We'd love to help you
                     </Heading>
 
                     <Center>
@@ -53,7 +53,28 @@ export default function Contact() {
                             We have a wonderful team of UX/UI humans who would love to help make your webpages intuitive and user-friendly.
                         </Text>
                     </Center>
+                </Box>
 
+                <Box width="100%">
+                    <Flex position="relative" backgroundColor="#E5E5E5" margin="auto" maxWidth="500px" minWidth="300px" height="400px" justifyContent="center" flexWrap="wrap" marginTop="40px">
+                        <Flex position="absolute" top="-7" justifyContent="center" flexWrap="wrap" width="100%"> 
+                            <svg width="104" height="64" viewBox="0 0 104 64" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <mask id="path-1-inside-1" fill="white">
+                            <rect width="104" height="64" rx="2"/>
+                            </mask>
+                            <rect width="104" height="64" rx="2" fill="white" stroke="black" stroke-width="7" mask="url(#path-1-inside-1)"/>
+                            <path d="M1.82471 2L49.5709 31.5235C52.2973 33.2094 53.6605 34.0523 55.163 34.0068C56.6655 33.9613 57.9752 33.0374 60.5946 31.1896L101.263 2.5" stroke="black" stroke-width="3.5" stroke-linecap="round" stroke-linejoin="round"/>
+                            <path d="M2.5 61L42.5 28.5" stroke="black" stroke-width="3.5"/>
+                            <path d="M101 61L65 29" stroke="black" stroke-width="3.5"/>
+                            </svg>
+                        </Flex>
+                        
+                        <Input placeholder="Email" borderBottom="1px solid #747474" borderRadius="0" mt="50px" width="80%"/>
+                        <Textarea placeholder="Message" borderBottom="1px solid #747474" borderRadius="0" height="45%" width="80%" resize="none"/>
+                        <Flex width="100%" justifyContent="center" flexWrap="wrap">
+                            <Button backgroundColor="black" color="#EEEDED" size="md" _hover={{backgroundColor:"black", color:"#EEEDED"}}> EMAIL US </Button>
+                        </Flex>
+                    </Flex>
                 </Box>
             </Stack>
         </Container>
